a85a29885ff1da52462dca2e755cc602b2269ed7
samade747 <samad.e747@gmail.com>
2024-06-01 11:12:23 +0500

    1112am 1 6 2024

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,20 @@
-console.log("i");
-setTimeout(() => {
-    console.log("eat");
-}, 0);
-console.log("icecreamm");
+function orderPizza(prepareCb) {
+    console.log("placing order");
+    setTimeout(() => {
+        console.log("order placed");
+        prepareCb(deliverycb);
+    }, 3000);
+}
+function prepearPizza(deliverycb) {
+    console.log("preparing pizza");
+    setTimeout(() => {
+        console.log("pizza is being prepared 🍕");
+        deliverycb();
+    }, 3000);
+}
+function deliverycb() {
+    console.log("delivering pizza");
+    console.log("lo khao pizza");
+}
+orderPizza(prepearPizza);
 //# sourceMappingURL=main.js.map
