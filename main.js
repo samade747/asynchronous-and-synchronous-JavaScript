38bfc23f1a9ca0a76f6e344c27e53e79c1ac24ae
samade747 <samad.e747@gmail.com>
2024-06-01 10:13:22 +0500

    1013am 1 6 2024

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
-// console only prints i
-console.log("i");
+// // console only prints i
+// console.log("i")
+// // async function with settimeout
+// setTimeout(() => {  
+//     console.log("eat")
+//     // async function with settimeout
+//     setTimeout(() => {  
+//         console.log("icecream")
+//     }, 1000)
+// }, 1000)
+// // console only prints icecreamm
+// // console.log("icecreamm")
+// // async function with loop 
+// function syncTask() {
+//     console.log("Task 1 started");
+//     // Simulating a blocking task, like a for-loop with a delay
+//     for (let i = 0; i < 1e9; i++) {}
+//     console.log("Task 1 completed");
+//     console.log("Task 2 started");
+//     console.log("Task 2 completed");
+//   }
+//   syncTask();
+//   console.log("All tasks completed");
 // async function with settimeout
-setTimeout(function () {
-    console.log("eat");
+function orderPizza() {
+    console.log("placing order");
     setTimeout(function () {
-        console.log("icecream");
-    }, 1000);
-}, 1000);
-// console only prints icecreamm
-// console.log("icecreamm")
+        console.log("order placed");
+    }, 3000);
+}
+orderPizza();
--- main.ts
+++ main.ts
@@ -1,21 +1,50 @@
 
-// console only prints i
-console.log("i")
+// // console only prints i
+// console.log("i")
 
-// async function with settimeout
-setTimeout(() => {  
-    console.log("eat")
+// // async function with settimeout
+// setTimeout(() => {  
+//     console.log("eat")
     
 
-    // async function with settimeout
-    setTimeout(() => {  
-        console.log("icecream")
-    }, 1000)
+//     // async function with settimeout
+//     setTimeout(() => {  
+//         console.log("icecream")
+//     }, 1000)
 
 
-}, 1000)
+// }, 1000)
 
 
-// console only prints icecreamm
-// console.log("icecreamm")
+// // console only prints icecreamm
+// // console.log("icecreamm")
+
+
+// // async function with loop 
+// function syncTask() {
+//     console.log("Task 1 started");
+//     // Simulating a blocking task, like a for-loop with a delay
+//     for (let i = 0; i < 1e9; i++) {}
+//     console.log("Task 1 completed");
+    
+//     console.log("Task 2 started");
+//     console.log("Task 2 completed");
+//   }
+  
+//   syncTask();
+//   console.log("All tasks completed");
+  
+
+// async function with settimeout
+
+function orderPizza() {
+    console.log("placing order")
+
+    setTimeout(() => {
+        console.log("order placed")
+    }, 3000)
+    
+}
+
+orderPizza()
 
